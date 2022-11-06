56b43e1b396fdc80dc381c4a754fadd45fa08451
keroro <keroroxx520@gmail.com>
2022-11-07 01:38:57 +0800

    feat(utils): add query_l1_header_by_timestamp

--- crates/utils/src/lib.rs
+++ crates/utils/src/lib.rs
@@ -1,4 +1,5 @@
 pub mod abort_on_drop;
+mod calc_finalizing_range;
 pub mod compression;
 pub mod exponential_backoff;
 pub mod export_block;
@@ -7,6 +8,7 @@ pub mod genesis_info;
 pub mod liveness;
 pub mod local_cells;
 pub mod polyjuice_parser;
+mod query_l1_header_by_timestamp;
 mod query_rollup_cell;
 pub mod script_log;
 pub mod since;
@@ -14,4 +16,6 @@ pub mod transaction_skeleton;
 pub mod wallet;
 pub mod withdrawal;
 
+pub use calc_finalizing_range::calc_finalizing_range;
+pub use query_l1_header_by_timestamp::{get_confirmed_header, query_l1_header_by_timestamp};
 pub use query_rollup_cell::query_rollup_cell;
